864d4ccf23a592cac4e6f466aad4163e9bd6b5d7
Eliseo ortiz <eliseo271099@gmail.com>
2022-10-07 14:02:21 -0500

    m

--- resources/views/vista_invitacion.blade.php
+++ resources/views/vista_invitacion.blade.php
@@ -673,8 +673,8 @@
         back.css("width", wi + "px");
         back.css("height", w2 + "px");
         var ff2 = $("#frontFlap2");
-        ff2.css("border-left", w1 + "px solid transparent");
-        ff2.css("border-bottom", (w1 * 58.8 | 0) + "px solid #cc3770");
+        ff2.css("border-left", wi + "px solid transparent");
+        ff2.css("border-bottom", (wi * 58.8 | 0) + "px solid #cc3770");
     </script>
     <script>
         $("#Envelope").mouseover(function() {
