55378abc7148eca58ba8591b604feac9632b668d
Eliseo ortiz <eliseo271099@gmail.com>
2022-10-07 15:41:08 -0500

    m

--- resources/views/vista_invitacion.blade.php
+++ resources/views/vista_invitacion.blade.php
@@ -691,7 +691,7 @@
         p.css("height", (wp * 0.60 | 0) + "px");
         p.css("padding-left", pp + "px");
         var img_f = $("#img_fondo_bolelto");
-        img_f.css("height", (wi * 1.2 | 0) + "px");
+        img_f.css("height", wi + "px");
     </script>
     <script>
         $("#Envelope").mouseover(function() {
