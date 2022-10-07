e37a9265a3853e6a31d3df84b387d298201852da
Eliseo ortiz <eliseo271099@gmail.com>
2022-10-07 01:32:18 -0500

    Using script for border with percentage

--- resources/views/vista_invitacion.blade.php
+++ resources/views/vista_invitacion.blade.php
@@ -659,7 +659,7 @@
         </a>
     </footer>
     <script>
-        var el = $("#OpenFlapBack");
+        var el = $("#shadow");
         var wi = el.width()
         var w = wi * .44 | 0; // calculate & trim decimals
         var w2 = wi * .35 | 0;
