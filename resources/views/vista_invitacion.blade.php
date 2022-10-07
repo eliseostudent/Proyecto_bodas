14bf70bf19bba930479932d04aa501a4502d8679
Eliseo ortiz <eliseo271099@gmail.com>
2022-10-07 11:53:01 -0500

    m

--- resources/views/vista_invitacion.blade.php
+++ resources/views/vista_invitacion.blade.php
@@ -659,7 +659,7 @@
         </a>
     </footer>
     <script>
-        var el = $("#Shadow");
+        var el = $("#OpenFlapBack");
         var wi = $("#Shadow").width();
         var w = wi * .44 | 0; // calculate & trim decimals
         var w2 = wi * .35 | 0;
