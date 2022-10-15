8b3b5e4b598261ffcba6f0516ad02be7521ce3ff
eliseooortiz <eliseo271099@gmail.com>
2022-10-15 18:25:42 -0500

    changing background of backflip

--- resources/views/vista_invitacion.blade.php
+++ resources/views/vista_invitacion.blade.php
@@ -132,7 +132,7 @@
             width: 265px;
             position: absolute;
             background-color: #7FB3D5;
-            background-image: url("/var/www/celi-y-eli.fun/public/app-images/brown-texture.jpg");
+            background-image: "url( " + {{ asset('app-images/brown-texture.jpg') }} + " )";
             /*
             margin-left: 264px;
             margin-top: 270px;
