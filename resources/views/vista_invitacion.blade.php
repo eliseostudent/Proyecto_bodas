f70d8f5036988a4e719368e85866c385e54af46e
eliseooortiz <eliseo271099@gmail.com>
2022-10-22 15:22:48 -0500

    adding images not by css

--- resources/views/vista_invitacion.blade.php
+++ resources/views/vista_invitacion.blade.php
@@ -338,14 +338,14 @@
                         <img src="{{ asset('app-images/boleto.jpg') }}"
                             alt="fondo boleto"style="width: 100%; height:100%">
                         <div style="position: absolute;top: 0%; left: 0%; width:100%; height:100%  ">
-                            <div class="row h-25 align-items-center">
+                            <div class="row h-25 " style="align-items: end;">
                                 <div class="col">
                                     <h1 class="text-center  cursiva" style="font-size: 1.5em;">
                                         Datos del invitado
                                     </h1>
                                 </div>
                             </div>
-                            <div class="row h-25 align-items-center">
+                            <div class="row h-25 " style="align-items: end;">
                                 <div class="col">
                                     <h1 class=" text-center baskerville" style="font-size: 1.5em;">
                                         {{ $invitado->nombre_invitado }}</h1>
