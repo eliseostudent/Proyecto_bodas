2bec0fa91fa448211c4783715502379e4dbb7afe
Eliseo ortiz <eliseo271099@gmail.com>
2022-10-30 16:48:01 -0600

    Cambio a descarga de excel

--- resources/views/exports/invitados_excel.blade.php
+++ resources/views/exports/invitados_excel.blade.php
@@ -3,6 +3,7 @@
         <tr>
             <th style="color: #5F9ACF">Nombre del invitado</th>
             <th style="color: #4D7BA4">Boletos</th>
+            <th style="color: #4D7BA4">Confirmacion</th>
             <th style="color: #4D7BA4">Url</th>
         </tr>
     </thead>
@@ -10,8 +11,15 @@
         @foreach ($invitados as $invitado)
             <tr>
                 <td style="color: #000">{{ $invitado->nombre_invitado }}</td>
-                <td style="color: #24FF00">{{ $invitado->numero_boletos }}</td>
-                <td style="color: #24FF00">https://celi-y-eli.fun/inv/{{ $invitado->hash }}</td>
+                <td style="color: #0040ff">{{ $invitado->numero_boletos }}</td>
+                @if ($invitado->confirmacion == '0')
+                    <td style="color: #24FF00">Asistire</td>
+                @elseif ($invitado->confirmacion == '1')
+                    <td style="color: #ff0000">No asistire</td>
+                @else
+                    <td style="color: #000">Por confirmar</td>
+                @endif
+                <td style="color: #0073ff">https://celi-y-eli.fun/inv/{{ $invitado->hash }}</td>
             </tr>
         @endforeach
     </tbody>
