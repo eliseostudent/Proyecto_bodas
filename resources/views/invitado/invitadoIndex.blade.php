cc8a503e18afab9be87880b9a0d3a85d98ce0152
Eliseo ortiz <eliseo271099@gmail.com>
2022-11-12 10:46:59 -0600

    tables

--- resources/views/invitado/invitadoIndex.blade.php
+++ resources/views/invitado/invitadoIndex.blade.php
@@ -1,17 +1,18 @@
 <x-app-layout>
 
     <x-slot name="estilos">
-        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.1/css/jquery.dataTables.min.css">
+        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.1/css/dataTables.bootstrap4.min.css">
+
+    </x-slot>
+    <x-slot name="tablescript">
+
+
+
 
         <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js">
         </script>
         <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.13.1/js/dataTables.bootstrap4.min.js">
         </script>
-
-    </x-slot>
-    <x-slot name="tablescript">
-
-        <script type="text/javascript" charset="utf8" src="https://code.jquery.com/jquery-3.5.1.js"></script>
         <script>
             $(document).ready(function() {
                 $('#index_table_invitados').DataTable();
