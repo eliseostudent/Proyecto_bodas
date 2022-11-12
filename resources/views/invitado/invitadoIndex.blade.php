ed33451daa3000c9e0a1025160fefaa81380c78a
Eliseo ortiz <eliseo271099@gmail.com>
2022-11-11 19:08:48 -0600

    cambios a la tablba de  invtiados

--- resources/views/invitado/invitadoIndex.blade.php
+++ resources/views/invitado/invitadoIndex.blade.php
@@ -1,19 +1,12 @@
 <x-app-layout>
 
     <x-slot name="estilos">
-        <link rel="stylesheet" type="text/css"
-            href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap5.min.css" />
-        <link rel="stylesheet" type="text/css"
-            href="https://cdn.datatables.net/responsive/2.3.0/css/responsive.dataTables.min.css" />
-
+        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.1/css/jquery.dataTables.css">
+  
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.js"></script>
 
     </x-slot>
     <x-slot name="tablescript">
-        <script src="https://code.jquery.com/jquery-3.6.1.min.js"
-    integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
-<script src="//cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
-<script src="//cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js"></script>
-<script src="//cdn.datatables.net/responsive/2.3.0/js/dataTables.responsive.min.js"></script>
         <script> $(document).ready(function() { $('.index_table_invitados').DataTable(); });</script>
     </x-slot>
     <x-slot name="header_mobile">
