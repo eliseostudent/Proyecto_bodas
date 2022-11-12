b9ed029e1c480433742b1258a5c47acf211521fb
Eliseo ortiz <eliseo271099@gmail.com>
2022-11-12 11:11:35 -0600

    tables

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -100,7 +100,7 @@
     @if (isset($scriptsExtras))
         {{ $scriptsExtras }}
     @endif
-    <!-- Jquery JS
+    <!-- Jquery JS -->
     <script src="{{ asset('cool_admin/vendor/jquery-3.2.1.min.js') }}"></script>
     <!-- Bootstrap JS-->
     <script src="{{ asset('cool_admin/vendor/bootstrap-4.1/popper.min.js') }}"></script>
