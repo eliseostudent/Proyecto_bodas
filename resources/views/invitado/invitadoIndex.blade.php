aa699e71f9475829d6078b8371c71bc6237a11db
Eliseo ortiz <eliseo271099@gmail.com>
2022-11-12 10:39:05 -0600

    tables

--- resources/views/invitado/invitadoIndex.blade.php
+++ resources/views/invitado/invitadoIndex.blade.php
@@ -2,15 +2,21 @@
 
     <x-slot name="estilos">
         <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.1/css/jquery.dataTables.min.css">
-  
-<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
-<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.13.1/js/dataTables.bootstrap4.min.js"></script>
+
+        <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js">
+        </script>
+        <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.13.1/js/dataTables.bootstrap4.min.js">
+        </script>
 
     </x-slot>
     <x-slot name="tablescript">
-        
-<script type="text/javascript" charset="utf8" src="https://code.jquery.com/jquery-3.5.1.js"></script>
-        <script> $(document).ready(function() { $('#index_table_invitados').DataTable(); });</script>
+
+        <script type="text/javascript" charset="utf8" src="https://code.jquery.com/jquery-3.5.1.js"></script>
+        <script>
+            $(document).ready(function() {
+                $('#index_table_invitados').DataTable();
+            });
+        </script>
     </x-slot>
     <x-slot name="header_mobile">
         @include('layouts.header_mobile_menu')
