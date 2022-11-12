995bbcfd27e6a771fafc03b85cc07f588dc164e6
Eliseo ortiz <eliseo271099@gmail.com>
2022-11-12 12:00:33 -0600

    tables

--- app/Http/Controllers/InvitadoController.php
+++ app/Http/Controllers/InvitadoController.php
@@ -24,7 +24,7 @@ class InvitadoController extends Controller
             abort(403);
         }
         Auth::user()->setEventoActual($evento);
-        $invitados = $evento->invitados->orderedBy('confirmacion','desc');
+        $invitados = $evento->invitados;
         return view('invitado/invitadoIndex', compact('invitados', 'evento'));
     }
 
