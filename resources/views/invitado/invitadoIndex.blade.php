adcb0a07f78f3a15a8099b131f15fb8f00c4908a
Eliseo ortiz <eliseo271099@gmail.com>
2022-11-11 19:12:13 -0600

    cambios a la tablba de  invtiados

--- resources/views/invitado/invitadoIndex.blade.php
+++ resources/views/invitado/invitadoIndex.blade.php
@@ -3,7 +3,9 @@
     <x-slot name="estilos">
         <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.1/css/jquery.dataTables.css">
   
-<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.js"></script>
+<script type="text/javascript" charset="utf8" src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.13.1/js/dataTables.bootstrap4.min.js"></script>
 
     </x-slot>
     <x-slot name="tablescript">
