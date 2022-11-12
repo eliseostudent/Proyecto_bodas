83920f5ae72c6731de43a91f287d99a7d852c9ef
Eliseo ortiz <eliseo271099@gmail.com>
2022-11-12 11:05:05 -0600

    tables

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -129,10 +129,7 @@
                 });
         }
     </script>
-    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js">
-    </script>
-    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.13.1/js/dataTables.bootstrap4.min.js">
-    </script>
+
     @if (isset($tablescript))
         {{ $tablescript }}
     @endif
