37452e33ad06f8f85a622945f6d2e959b67bb80e
Eliseo ortiz <eliseo271099@gmail.com>
2022-11-12 11:41:36 -0600

    tables

--- resources/views/invitado/invitadoIndex.blade.php
+++ resources/views/invitado/invitadoIndex.blade.php
@@ -115,7 +115,7 @@
     </div>
     <x-slot name="tablescript">
         <script src="//code.jquery.com/jquery-3.5.1.js"></script>
-        <script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
+        <script src="//cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
 
         <script>
             $(document).ready(function() {
